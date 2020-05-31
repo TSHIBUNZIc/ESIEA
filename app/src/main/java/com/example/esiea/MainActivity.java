0d998dd4f90c55c2646a50e878a460edb902c48b
TSHIBUNZI <tshibunzi@gmail.com>
2020-05-31 14:54:58 +0200

    ajout de la liste

--- app/src/main/java/com/example/esiea/MainActivity.java
+++ app/src/main/java/com/example/esiea/MainActivity.java
@@ -1,23 +1,43 @@
 package com.example.esiea;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends BaseActivity implements MainInterface  {
+public class MainActivity extends AppCompatActivity
+{
+    private RecyclerView recyclerView;
+    private ListAdapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button mainButton = findViewById(R.id.main_button);
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setHasFixedSize(true);
+    // use a linear layout manager
+    layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+    List<String> input = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+    input.add("Test" + i);
+}// define an adapter
+    mAdapter = new ListAdapter(input);
+        recyclerView.setAdapter(mAdapter);
+}
+
+        //Button mainButton = findViewById(R.id.main_button);
          //mainButton
-        mainButton.setOnClickListener(new View.OnClickListener() {
+       /* mainButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Toast.makeText(MainActivity.this,"Onclicked", Toast.LENGTH_LONG).show();
@@ -25,11 +45,11 @@ public class MainActivity extends BaseActivity implements MainInterface  {
             }
         });
         Film film = new Film("lion" ,100 );
-        showBaseError();
+        showBaseError();*/
     }
 
-    @Override
-    public void showList(List<Film> list) {
+
+    /*public void showList(List<Film> list) {
 // mettre le vrai code ici
     }
 
@@ -41,5 +61,5 @@ public class MainActivity extends BaseActivity implements MainInterface  {
     @Override
     public void showError() {
 
-    }
-}
+    }*/
+
